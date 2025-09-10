c96a7eb76e97537918b786ed25f8d8ada076eb21
LuisHenrique004 <luishenrique004si@gmail.com>
2025-09-10 18:00:54 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { CursosService } from './services/cursos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angularCurso';
+
+  cursos: string[] = [];
+
+  constructor(private cursosService: CursosService) {
+    this.cursos = this.cursosService.getCursos();
+
+  }
 }
